cd4d8f66c5c596d7ffacdd1de52f4e734379412b
Chris Nuernberger <chris@techascent.com>
2020-05-23 08:27:21 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tech.ml.dataset.sql "0.1"
+(defproject tech.ml.dataset.sql "0.2-SNAPSHOT"
   :description "SQL bindings for tech.ml.dataset"
   :url "https://github.com/techascent/tech.ml.dataset.sql"
   :license {:name "EPL-2.0"
