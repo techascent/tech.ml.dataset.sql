38c4db994a49211e5f5168225d9b377539abcbb4
Chris Nuernberger <chris@techascent.com>
2020-08-10 07:19:52 -0600

    Testing with upgrade tech.ml.dataset.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :dependencies [[techascent/tech.ml.dataset "3.02" :scope "provided"]]
+  :dependencies [[techascent/tech.ml.dataset "4.02" :scope "provided"]]
   :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}
                    ;;Word to the wise; if you aren't developing on the dataset code
                    ;;itself then precompile it.
